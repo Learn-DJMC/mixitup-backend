2ed3068919ea4d967d51d74e74bc79fa06cb12d7
TheLavitz <lavitz@gmail.com>
2024-01-08 16:03:48 -0800

    Controller method for create with strong_params and test coverage

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,12 @@
 class RecipesController < ApplicationController
+
+    def create
+        recipe = Recipe.create!(recipe_params)
+        render json: recipe
+    end
+
+    private
+    def recipe_params
+        params.require(:recipe).permit(:title, :category, :dietary_restrictions, :rating, :description, :image, :user_id, ingredients:[], instructions:[])
+    end
 end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Recipes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /create" do
+    it "creates a recipe" do
+      user = User.create(email: "test@example.com", username: "test", password: "password")
+      recipe_params = {
+        recipe: {
+          title: "Recipe 1",
+          category: "default",
+          dietary_restrictions: "default",
+          rating: 0,
+          description: "Default description",
+          ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"],
+          instructions: ["Step 1", "Step 2", "Step 3"],
+          image: "recipe1",
+          user_id: user.id
+        }
+      }
+    post "/recipes", params: recipe_params
+    expect(response).to have_http_status(200)
+    recipe = Recipe.first
+    expect(recipe.title).to eq "Recipe 1"
+    puts "Recipe: #{recipe.ingredients}"
+    end
   end
 end
