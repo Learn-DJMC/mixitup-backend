697b04ef81f98868966b5a683f0f6222bb7150c3
jacob <jacobamezola@gmail.com>
2024-01-09 13:13:24 -0800

    added read functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@
 user1 = User.where(email: "test1@example.com", username: "test1").first_or_create(password: "password", password_confirmation: "password")
 user2 = User.where(email: "test2@example.com", username: "test2").first_or_create(password: "password", password_confirmation: "password")
 
-recipe1 = Recipe.create!(title: "Recipe 1", category: "", dietary_restrictions: "", rating: 0, description: "Default description", ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"], instructions: ["Step 1", "Step 2", "Step 3"], image: "recipe1", user_id: user1.id)
-recipe2 = Recipe.create!(title: "Recipe 2", category: "", dietary_restrictions: "", rating: 0, description: "Default description", ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"], instructions: ["Step 1", "Step 2", "Step 3"], image: "recipe2", user_id: user2.id)
+recipe1 = Recipe.create!(title: "Recipe 1", category: "default", dietary_restrictions: "default", rating: 0, description: "Default description", ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"], instructions: ["Step 1", "Step 2", "Step 3"], image: "recipe1", user_id: user1.id)
+recipe2 = Recipe.create!(title: "Recipe 2", category: "default", dietary_restrictions: "default", rating: 0, description: "Default description", ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"], instructions: ["Step 1", "Step 2", "Step 3"], image: "recipe2", user_id: user2.id)
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,6 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Recipes", type: :request do
+  describe "Get /index" do
+    it 'gets a list of recipes' do
+      user = User.create(email: "test@example.com", username: "test", password: "password")
+      Recipe.create(
+        title: "recipe 1",
+        category: "default",
+        dietary_restrictions: "default",
+        rating: 0,
+        description: "default description",
+        ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"],
+        instructions: ["Step 1", "Step 2", "Step 3"],
+        image: "recipe1",
+        user_id: user.id
+      )
+      get '/recipes'
+  
+      recipe = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(recipe.first['title']).to eq('recipe 1')
+      expect(recipe.first['category']).to eq('default')
+      expect(recipe.first['dietary_restrictions']).to eq('default')
+      expect(recipe.first['rating']).to eq(0)
+      expect(recipe.first['description']).to eq('default description')
+      expect(recipe.first['ingredients']).to eq(["Ingredient 1", "Ingredient 2", "Ingredient 3"])
+      expect(recipe.first['instructions']).to eq(["Step 1", "Step 2", "Step 3"])
+      expect(recipe.first['image']).to eq('recipe1')
+    end
+  end
+
   describe "POST /create" do
     it "creates a recipe" do
       user = User.create(email: "test@example.com", username: "test", password: "password")
@@ -197,4 +226,5 @@ RSpec.describe "Recipes", type: :request do
       expect(recipe["user_id"]).to include "can't be blank"
     end
   end
+
 end
