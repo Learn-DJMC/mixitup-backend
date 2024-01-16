e0792f697e7689a72fa8cafdeff24cf305fc98f1
TheLavitz <lavitz@gmail.com>
2024-01-16 14:09:05 -0800

    added URL to cors

--- config/cors.rb
+++ config/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'http://localhost:3001'
+      # origins 'mixitup.onrender.com'
       resource '*',
       headers: ["Authorization"],
       expose: ["Authorization"],
